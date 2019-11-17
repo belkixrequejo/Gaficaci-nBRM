334da3811d3cf3ad4a5e8351c60105a5358fadfe
belkixrequejo <belkix.requejo.edu.ec>
2019-11-17 07:54:54 -0500

    Blog

---
+++ HTML5/enviar2.php
@@ -0,0 +1,17 @@
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$tema = $_POST['tema'];97
+
+$mensajes = $_POST['mensaje'];
+
+
+
+
+$para = 'belkix.requejo@pucese.edu.ec';
+$asunto = 'Mensaje de mi sitio web';
+
+mail($para, $asunto, $mensaje);
+header("Location:contact.html");
+
+?>
